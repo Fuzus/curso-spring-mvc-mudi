586cf3b27d08dbda165c0cfd8dfd6d0d542dcabc
Fuzus <matheusdamata2000@gmail.com>
2023-01-04 20:18:44 -0300

    Adicionando um pedido fake para pegar os valores dinamicamete no html

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,26 @@
 package br.com.alura.mvc.mudi.controller;
 
+import br.com.alura.mvc.mudi.model.Order;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        Order order = new Order();
+        order.setName("Smartphone Xiaomi Redmi Note 8");
+        order.setImageURL("https://m.media-amazon.com/images/I/51RIqTiRc8L._AC_SX679_.jpg");
+        order.setProductURL("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-Camera/dp/B07Z5BBG56/ref=sr_1_1?keywords=redmi+8&qid=1672873627&sr=8-1");
+        order.setDescription("Uma descricao qualquer para esse pedido");
+
+        List<Order> list =  Arrays.asList(order);
+        model.addAttribute("orders", list);
         return "home";
     }
 }
---
+++ src/main/java/br/com/alura/mvc/mudi/model/Order.java
@@ -0,0 +1,62 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Order {
+
+    private String name;
+    private BigDecimal value;
+    private LocalDate deliveryDate;
+    private String productURL;
+    private String imageURL;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getProductURL() {
+        return productURL;
+    }
+
+    public void setProductURL(String productURL) {
+        this.productURL = productURL;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
