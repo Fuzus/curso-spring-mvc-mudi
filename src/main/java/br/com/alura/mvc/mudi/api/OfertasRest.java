5023528a4bf06a85dd4c3d9bdf37e645b10ab1ef
Fuzus <matheusdamata2000@gmail.com>
2023-01-16 17:10:38 -0300

    criando novas ofertas

--- src/main/java/br/com/alura/mvc/mudi/api/OfertasRest.java
+++ src/main/java/br/com/alura/mvc/mudi/api/OfertasRest.java
@@ -6,6 +6,7 @@ import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class OfertasRest {
     private PedidoRepository pedidoRepository;
 
     @PostMapping
-    public Offer createOferta(NewOfferRequest request) {
+    public Offer createOferta(@RequestBody NewOfferRequest request) {
 
         Optional<Pedido> pedidoBuscado = pedidoRepository.findById(request.getPedidoId());
         if (!pedidoBuscado.isPresent()) {
